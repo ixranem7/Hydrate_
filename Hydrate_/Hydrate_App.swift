f2b925c2a67bfc6a06aaf249f4cf9f553ef64360
raneem <ixranem7@gmail.com>
2024-10-21 11:04:58 +0300

    Initial Commit

---
+++ Hydrate_/Hydrate_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hydrate_App.swift
+//  Hydrate_
+//
+//  Created by Raneem on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Hydrate_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hydrate_Tests/Hydrate_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Hydrate_Tests.swift
+//  Hydrate_Tests
+//
+//  Created by Raneem on 18/04/1446 AH.
+//
+
+import Testing
+@testable import Hydrate_
+
+struct Hydrate_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
